c0a689afe38ecdb036854f591779517f827c0c88
mdeift <mdeift@gmail.com>
2013-11-20 18:05:48 +0200

    test

--- UserDbOperations.py
+++ UserDbOperations.py
@@ -15,18 +15,24 @@ from Logger import ERROR, WARNING, INFO, Log
 class UserOperations:
 
     route_user = '/_user'
+    route_connect = '/_user/connect'
+    route_disconnect = '/_user/disconnect'
+    route_get_connected_list = '/_user/get_connected_list'
     
     def __init__(self, db):
         self.db = db
         route(self.route_user, method='PUT')(self.registerUser)
         route(self.route_user, method='DELETE')(self.unregisterUser)
+        route(self.route_connect, method='PUT')(self.connectUser)
+        route(self.route_disconnect, method='PUT')(self.disconnectUser)
+        route(self.route_get_connected_list, method='GET')(self.getConnectedUsers)
         
-    #@route(route_user, method='PUT', )
+    #@route(route_user, method='POST', )
     def registerUser(self):      
         req = request.json
         name = req['name']
         
-        Log(INFO, 'Received request. \"name\"= ' + name )
+        Log(INFO, 'Received request = ' + str(req) )
               
         isExist, obj = self.isUserExistInDb('name', name)
         if isExist:
@@ -47,6 +53,8 @@ class UserOperations:
         req = request.json
         name = req['name']
         
+        Log(INFO, 'Received request = ' + str(req) )
+        
         isExist, obj = self.isUserExistInDb('name', name)
         if not isExist:
             theBody = json.dumps({'reason': 'Not exist user '+name})
@@ -59,6 +67,21 @@ class UserOperations:
         theBody = json.dumps({'reason': 'Deleted'})
         Log(INFO, 'Deleted user \"name\"= ' + name )
         return HTTPResponse(status=200, body=theBody)
+    
+    #@route(route_connect, method='PUT', )
+    def connectUser(self):
+        req = request.json
+        Log(INFO, 'Received request = ' + str(req) )
+                            
+    #@route(route_disconnect, method='PUT', )
+    def disconnectUser(self):
+        req = request.json
+        Log(INFO, 'Received request = ' + str(req) )
+        
+    #@route(route_get_connected_list, method='GET', )
+    def getConnectedUsers(self):
+        Log(INFO, 'Received request' )
+        
     
     def isUserExistInDb(self, key, val):
         b, obj = self.db.find(key, val)
